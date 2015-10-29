d2324392fc30a2b585c26149e21f10586dbf097d
Bartosz Cieśla <bartociesla@gmail.com>
2015-10-29 15:39:28 +0100

    Dodano wąsy

--- PEA_PROJEKT/PEA_PROJEKT/main.cpp
+++ PEA_PROJEKT/PEA_PROJEKT/main.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	for(int i = 0; i< 10; ++i) // Dodana linia
-	cout << "Test" << endl;
+	for (int i = 0; i < 10; ++i)
+	{
+		// Dodana linia
+		cout << "Test" << endl;
+	}
+
 	return 0;
 }
